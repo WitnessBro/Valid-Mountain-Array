4c0ef56e0f434f204c073c108e64722192997f08
wtfbro <wtfbro@yandex-team.ru>
2024-09-22 15:32:28 +0300

    add missing part

--- test_test.go
+++ test_test.go
@@ -1,1 +1,36 @@
 package Valid_Mountain_Array
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	//var nums = []int{1, 2, 3, 2, 1}             // Input array 	// Value to remove
+	var nums = []int{3, 2, 1}           // Input array 	// Value to remove
+	var expectedFlag = true             // The expected answer with correct length.
+	var flag = validMountainArray(nums) // Calls your implementation
+
+	assert.Equal(t, flag, expectedFlag)
+}
+func validMountainArray(arr []int) bool {
+	peak := false
+	if len(arr) < 3 || arr[0] > arr[1] {
+		return false
+	}
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] == arr[i+1] {
+			return false
+		}
+		if arr[i] < arr[i+1] && peak == false {
+			continue
+		}
+		if arr[i] < arr[i+1] && peak == true {
+			return false
+		}
+		if arr[i] > arr[i+1] {
+			peak = true
+		}
+	}
+	return peak
+}
